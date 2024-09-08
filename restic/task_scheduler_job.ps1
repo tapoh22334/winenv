d9b6e701964731f07585ef90ad9ee7480186f2d3
iwase22334 <iwase22334@gmail.com>
2024-09-08 18:57:19 +0900

    add picture backup to restic

--- restic/task_scheduler_job.ps1
+++ restic/task_scheduler_job.ps1
@@ -1,6 +1,9 @@
 Push-Location $PSScriptRoot
 
 .\02_backup.ps1 repository_BackupB target-directory-all
-
 $filename = Get-Date -Format "yyyy-MMdd-HHmmss"
-.\03_print_snapshot.ps1 repository_BackupB >> C:\log\log_$filename.txt
+.\03_print_snapshot.ps1 repository_BackupB >> C:\log\restic-ddrive-all\log_$filename.txt
+
+.\02_backup.ps1 repository_BackupA target-directory-picture
+$filename = Get-Date -Format "yyyy-MMdd-HHmmss"
+.\03_print_snapshot.ps1 repository_BackupA >> C:\log\restic-picture\log_$filename.txt
